0508fa08a2ffc8857d98590ce50b650a84df877e
Nikhil002531 <nikhil9532007@gmail.com>
2024-11-23 10:37:28 +0530

    First commit

---
+++ Backend/EduMatrix/src/main/java/p1/EduMatrix/EduMatrixApplication.java
@@ -0,0 +1,13 @@
+package p1.EduMatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduMatrixApplication.class, args);
+	}
+
+}
---
+++ Backend/EduMatrix/src/test/java/p1/EduMatrix/EduMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package p1.EduMatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduMatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
